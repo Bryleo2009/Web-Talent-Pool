7611393712dd0d2640ef0dabb3a1ef381220582d
Bryan Morán <bryleo2009@hotmail.com>
2023-01-12 15:52:37 -0500

    Header + Home

--- pages/en/header.php
+++ pages/en/header.php
@@ -30,7 +30,7 @@
   <header class="d-flex justify-content-center">
     <nav class="navbar navbar-expand-lg bg-light row animate__animated animate__fadeInDown">
       <div class="container-fluid">
-        <a class="navbar-brand" href="index.php">Talent Pool</a>
+        <a class="navbar-brand" href="index.php"><img src="../../images/Header/Logo Talent pool-01 2.png" alt=""></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="true" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
--- pages/en/index.php
+++ pages/en/index.php
@@ -5,6 +5,9 @@
   <!--Info-->
   <section id="info" class="seccion">
     <div class="s-info row">
+      <div class="s-info-logo col-6 d-flex align-items-center justify-content-center">
+        <img src="../../images/Home/undraw_co-working_re_w93t 2.png" alt="">
+      </div>
       <div class="s-info-text col-6">
         <h1 class="animate__animated animate__slideInLeft">Looking for foreing workers?</h1>
         <p>Do you have a staff shortage for the upcoming tourist
@@ -13,17 +16,27 @@
           partner in Lima we will help you find workers from Peru.</p>
         <a href="looking.php"><button class="hvr-bounce-to-right">Request</button></a>
       </div>
-      <div class="s-info-logo col-6 d-flex align-items-center justify-content-center">
-        <img src="../../images/Home/img1.png" alt="">
-      </div>
+    </div>
+    <div class="abst-1">
+      <img src="../../images/Home/Abstract-3.png" alt="">
     </div>
   </section>
+
+  <!--Abstract-->
+  <div>
+    <div class="abst-2">
+      <img src="../../images/Home/Abstract-2.png" alt="">
+    </div>
+    <div class="abst-3">
+      <img src="../../images/Home/Abstract-1.png" alt="">
+    </div>
+  </div>
 
   <!--Procedure-->
   <section id="proce" class="seccion">
     <div class="s-proce row">
-      <h1 data-aos="fade-up" >Procedure</h1>
-      <div class="s-proce-text col-5">
+      <h1 data-aos="fade-up" class="text-center">Procedure</h1>
+      <div class="s-proce-text col-12">
         <p>All you need to do is to fill in the form for the
           required worker profile, and we will do the
           recruitment. After we send you the CVs of the
@@ -33,53 +46,40 @@
           will be started.
         </p>
       </div>
-      <div class="s-proce-items col-7">
-        <div class="row align-items-center" data-aos="zoom-out-down" >
-          <div class="s-proce-img col-4">
-            <img src="../../images/Home/img2.png" alt="">
-          </div>
-          <div class="s-proce-item_text col-8">
-            <h1>1. Request for the desired profile</h1>
-            <p>Occupation, job description, number of
-              candidates, work experience, education
-              and job-specific skills.</p>
-          </div>
+      <div class="row s-proce-items col-12">
+        <div class="s-proce-item-content col-3">
+          <h2>1</h2>
+          <h1>Request for the desired profile</h1>
+          <p>Occupation, job description, number of
+            candidates, work experience, education
+            and job-specific skills.</p>
         </div>
-        <div class="row align-items-center" data-aos="zoom-out-down" >
-          <div class="s-proce-img col-4">
-            <img src="../../images/Home/img3.png" alt="">
-          </div>
-          <div class="s-proce-item_text col-8">
-            <h1>2. Selection of candidates</h1>
-            <p>We select candidates for the required
-              profile and deliver selected CVs to the
-              client. Clients have the option of video
-              interviews with shortlisted candidates.</p>
-          </div>
+        <div class="s-proce-item-content col-3">
+          <h2>2</h2>
+          <h1>Selection of candidates</h1>
+          <p>We select candidates for the required
+            profile and deliver selected CVs to the
+            client. Clients have the option of video
+            interviews with shortlisted candidates.</p>
         </div>
-        <div class="row align-items-center" data-aos="zoom-out-down" >
-          <div class="s-proce-img col-4">
-            <img src="../../images/Home/img4.png" alt="">
-          </div>
-          <div class="s-proce-item_text col-8">
-            <h1>3. Work permits</h1>
-            <p>We collect the documentation from
-              candidates that is required for obtaining a
-              work permit. Visas are not required for
-              citizens of Peru.</p>
-          </div>
+        <div class="s-proce-item-content col-3">
+          <h2>3</h2>
+          <h1>Work permits</h1>
+          <p>We collect the documentation from
+            candidates that is required for obtaining a
+            work permit. Visas are not required for
+            citizens of Peru.</p>
         </div>
-        <div class="row align-items-center" data-aos="zoom-out-down"  style="padding-bottom: 0 !important;">
-          <div class="s-proce-img col-4">
-            <img src="../../images/Home/img5.png" alt="">
-          </div>
-          <div class="s-proce-item_text col-8">
-            <h1>4. Arrival of candidates</h1>
-            <p>We provide support with the candidate's
-              travel arrangements.</p>
-          </div>
+        <div class="s-proce-item-content col-3">
+          <h2>4</h2>
+          <h1>Arrival of candidates</h1>
+          <p>We provide support with the candidate's
+            travel arrangements.</p>
         </div>
       </div>
+    </div>
+    <div class="abst-4">
+      <img src="../../images/Home/Abstract-1.png" alt="">
     </div>
   </section>
 
@@ -87,52 +87,49 @@
   <section id="reque" class="seccion">
     <div class="s-reque row">
       <div class="s-reque-title">
-        <h1 data-aos="fade-up" >Send request</h1>
-        <p>To receive a project estimate, fill out the form <br>
+        <h1 data-aos="fade-up" class="text-center">Send request</h1>
+        <p class="text-center">To receive a project estimate, fill out the form
           and we'll get in touch with you.</p>
       </div>
       <div class="s-reque-forms row">
-        <form class="form-group col-6" method="post" action="../../ofsystem/enviar_solicitud.php" id="forms" enctype="multipart/form-data">
-          <label for="inp" class="inp" data-aos="zoom-in" >
+        <form class="form-group request-home col-12" method="post" action="../../ofsystem/enviar_solicitud.php" id="forms" enctype="multipart/form-data">
+          <label for="inp" class="inp" data-aos="zoom-in">
             <input type="text" id="employer" name="employer" required="" placeholder="&nbsp;">
             <span class="label">Employer</span>
             <span class="focus-bg"></span>
           </label>
-          <label for="inp" class="inp" data-aos="zoom-in" >
+          <label for="inp" class="inp" data-aos="zoom-in">
             <input type="email" id="email" name="email" required="" placeholder="&nbsp;">
             <span class="label">E-mail</span>
             <span class="focus-bg"></span>
           </label>
-          <label for="inp" class="inp" data-aos="zoom-in" >
+          <label for="inp" class="inp" data-aos="zoom-in">
             <input type="text" id="cel" name="cel" required="" placeholder="&nbsp;">
             <span class="label">Contact number</span>
             <span class="focus-bg"></span>
           </label>
-          <label for="inp" class="inp" data-aos="zoom-in" >
+          <label for="inp" class="inp" data-aos="zoom-in">
             <input type="text" id="name" name="name" required="" placeholder="&nbsp;">
             <span class="label">Name</span>
             <span class="focus-bg"></span>
           </label>
-          <label for="inp" class="inp" data-aos="zoom-in" >
+          <label for="inp" class="inp" data-aos="zoom-in">
             <input type="text" id="position" name="position" required="" placeholder="&nbsp;">
             <span class="label">Position</span>
             <span class="focus-bg"></span>
           </label>
-          <label for="inp" class="inp" data-aos="zoom-in" >
-          <input type="number" min="1" max="100"id="number" name="number" required="" placeholder="&nbsp;">
+          <label for="inp" class="inp" data-aos="zoom-in">
+            <input type="number" min="1" max="100" id="number" name="number" required="" placeholder="&nbsp;">
             <span class="label">Required number of workers</span>
             <span class="focus-bg"></span>
           </label>
-          <label for="inp" class="inp" data-aos="zoom-in" >
+          <label for="inp" class="inp" data-aos="zoom-in">
             <textarea type="text" id="msg" name="msg" required="" placeholder="&nbsp;" rows="4" cols="50"></textarea>
             <span class="label">Message</span>
             <span class="focus-bg"></span>
           </label>
-          <button  data-aos="zoom-in" class="hvr-bounce-to-right" type="submit" name="enviar" id="btn" onclick="modal('enviar','forms')" disabled>Send</button>
+          <button data-aos="zoom-in" class="hvr-bounce-to-right" type="submit" name="enviar" id="btn" onclick="modal('enviar','forms')" disabled>Send</button>
         </form>
-        <div class="s-reque-forms-logo col-6 d-flex align-items-center">
-          <img src="../../images/Home/img6.png" alt="">
-        </div>
       </div>
     </div>
   </section>
@@ -150,7 +147,7 @@
     t4 = document.getElementById("name").value.length;
     t5 = document.getElementById("position").value.length;
     t6 = document.getElementById("number").value;
-    if (t1 >= 3 && t2 >= 3 && t3 >= 5 && t4 >= 3 && t5 >= 3 && t6 >0) {
+    if (t1 >= 3 && t2 >= 3 && t3 >= 5 && t4 >= 3 && t5 >= 3 && t6 > 0) {
       document.getElementById("btn").disabled = false;
     } else {
       document.getElementById("btn").disabled = true;
