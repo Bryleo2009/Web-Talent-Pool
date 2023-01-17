d1562918a318dd06b1d751aadb4e6876ee335060
Bryan Moran <bmoran@certicom.com.pe>
2023-01-17 12:16:02 -0500

    favicon

--- pages/hr/header.php
+++ pages/hr/header.php
@@ -8,6 +8,24 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="author" content="Bryan A. Morán Vega">
   <meta name="copyright" content="Of System ®">
+  <!--faticon-->
+  <link rel="apple-touch-icon" sizes="57x57" href="../../images/Movil/apple-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="../../images/Movil/apple-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="../../images/Movil/apple-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="../../images/Movil/apple-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="../../images/Movil/apple-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="../../images/Movil/apple-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="../../images/Movil/apple-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="../../images/Movil/apple-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="../../images/Movil/apple-icon-180x180.png">
+  <link rel="icon" type="image/png" sizes="192x192" href="../../images/Movil/android-icon-192x192.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="../../images/Movil/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="../../images/Movil/favicon-96x96.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="../../images/Movil/favicon-16x16.png">
+  <link rel="manifest" href="../../images/Movil/manifest.json">
+  <meta name="msapplication-TileColor" content="#003A6F">
+  <meta name="msapplication-TileImage" content="../../images/Movil/ms-icon-144x144.png">
+  <meta name="theme-color" content="#003A6F">
   <!--íconos de la web-->
   <script src="https://kit.fontawesome.com/e3a4a2320e.js" crossorigin="anonymous"></script>
   <script src="https://cdn.lordicon.com/libs/mssddfmo/lord-icon-2.1.0.js"></script>
@@ -17,8 +35,7 @@
   <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet" media="all">
   <link rel="stylesheet" href="../../css/hover.css">
   <!--icono, css, js-->
-  <link rel="icon" href="../../images/SystemLogo.ico">
-
+  <link rel="icon" href="../../images/Movil/favicon.ico">
   <!-- Bootstrap Core CSS -->
   <!-- CSS only -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
@@ -29,7 +46,7 @@
 
 <body>
   <header class="d-flex justify-content-center">
-    <nav class="navbar navbar-expand-lg bg-light fixed-top row animate__animated animate__fadeInDown">
+    <nav class="navbar navbar-expand-lg bg-light row animate__animated animate__fadeInDown">
       <div class="container-fluid">
         <a class="navbar-brand" href="index.php"><img src="../../images/Header/Logo Talent pool-01 2.png" alt=""></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="true" aria-label="Toggle navigation">
